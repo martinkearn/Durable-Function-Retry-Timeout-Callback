19dc0c4cff4b7645779332b3f16e86a0b08c09b6
Martin Kearn <martin.kearn@microsoft.com>
2021-07-07 11:59:57 +0100

    Add RetryCounterEntity

---
+++ RetryTimeoutCallback/FunctionApp/Entities/RetryCounterEntity.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.DurableTask;
+
+namespace FunctionApp.Entities
+{
+    public class RetryCounterEntity
+    {
+        [FunctionName(nameof(RetryCounterEntity))]
+        public static void Counter([EntityTrigger] IDurableEntityContext ctx)
+        {
+            switch (ctx.OperationName.ToLowerInvariant())
+            {
+                case "increment":
+                    ctx.SetState(ctx.GetState<int>() + 1);
+                    break;
+                case "get":
+                    ctx.Return(ctx.GetState<int>());
+                    break;
+            }
+        }
+    }
+}
--- RetryTimeoutCallback/FunctionApp/Orchestrators/MainOrchestrator.cs
+++ RetryTimeoutCallback/FunctionApp/Orchestrators/MainOrchestrator.cs
@@ -1,6 +1,5 @@
-using System.Collections.Generic;
 using System.Threading.Tasks;
-using FunctionApp.Activities;
+using FunctionApp.Entities;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.DurableTask;
 
@@ -9,17 +8,24 @@ namespace FunctionApp.Orchestrators
     public static class MainOrchestrator
     {
         [FunctionName(nameof(MainOrchestrator))]
-        public static async Task<List<string>> RunOrchestrator([OrchestrationTrigger] IDurableOrchestrationContext context)
+        public static async Task<string> RunOrchestrator([OrchestrationTrigger] IDurableOrchestrationContext context)
         {
-            var outputs = new List<string>
-            {
-                // Replace "hello" with the name of your Durable Activity Function.
-                await context.CallActivityAsync<string>(nameof(SayHelloActivity), "Tokyo"),
-                await context.CallActivityAsync<string>(nameof(SayHelloActivity), "Seattle"),
-                await context.CallActivityAsync<string>(nameof(SayHelloActivity), "London")
-            };
+            var retryLimit = 3;
 
-            return outputs;
+            // Setup entity ID key'd from the orchestration instance id. One counter per orchestration instance.
+            var retryCounterEntityId = new EntityId(nameof(RetryCounterEntity), context.InstanceId);
+
+            // Increment counter until it is equal to the retryLimit
+            do
+            {
+                // do work here
+                context.SignalEntity(retryCounterEntityId, "increment");
+            } while (await context.CallEntityAsync<int>(retryCounterEntityId, "get") < retryLimit);
+
+            // Get final counter value.
+            var finalRetryCounterValue = await context.CallEntityAsync<int>(retryCounterEntityId, "get");
+
+            return $"Final count {finalRetryCounterValue}";
         }
     }
 }
